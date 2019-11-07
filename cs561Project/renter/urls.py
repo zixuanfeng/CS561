0fa9e04f7a7c73e67ceb710ec9d3716e2732d184
MikhailTokarev <43589096+MikhailTokarev@users.noreply.github.com>
2019-11-07 01:14:57 -0800

    Add files via upload

--- cs561Project/renter/urls.py
+++ cs561Project/renter/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path
 from django.conf.urls import url
-
 from . import views
-app_name = 'renter'
+from . import views
 
 urlpatterns = [
     path('renter_view/', views.renter_view, name="renter_view"),
     path('renter_list/', views.renter_list, name="renter_list"),
-    #path('payment_page/', views.payment_page, name="payment_page"),
-    url(r'^renter_list/', views.order_check, name='order_check'),
-
+    path('payment_page/', views.payment_page, name="payment_page"),
+    url(r'^renter_list/(?P<warehouse_id>[0-9]+)$', views.order_check, name='order_check'),
+    path('get_password/', views.get_password, name="get_password"),
     ]
--- cs561Project/renter/views.py
+++ cs561Project/renter/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 #from django.shortcuts import loader
 # Create your views here.
-
-from django.http import HttpResponse
-from account.models import Warehouse
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from .models import Warehouse
+import random
 
 @login_required
 def renter_view(request):
@@ -19,10 +19,22 @@ def renter_list(request):
 
     return render(request, 'renter/renter_list.html', context)
 @login_required
-def order_check(request):
-    all_warehouses = Warehouse.objects.all()
-    context = {'all_warehouses': all_warehouses}
-    return render(request, 'renter/order_check.html')
+def order_check(request, warehouse_id):
+    TheWarehouse = Warehouse.objects.get(warehouse_id = warehouse_id)
+    context = {'The_Warehouse' : TheWarehouse}
+    return render(request, 'renter/order_check.html', context )
+
+@login_required
+def payment_page(request):
+    return render(request, 'renter/payment_page.html')
+
+
+@login_required
+def get_password(request):
+    raran = random.randint(0, 999999999)
+    context = {'raran' : raran}
+    return render(request, 'renter/get_password.html' , context)
+
 
 
 #def detail(request, warehouse_id):
