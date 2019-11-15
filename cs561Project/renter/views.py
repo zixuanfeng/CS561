ebc10386474cd778cc3d1bde07bda74a7af3714c
littleKitchen <cheyuxuanll@gmail.com>
2019-11-15 03:59:18 -0800

    renter app updated

--- cs561Project/renter/views.py
+++ cs561Project/renter/views.py
@@ -3,12 +3,34 @@ from django.shortcuts import render
 # Create your views here.
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
-from .models import Warehouse
-from .models import User
-from .models import RentOrder
-import random
+from account.models import Warehouse
+from account.models import User
+from account.models import RentOrder
+import random 
 from django.utils.timezone import datetime
+from django.utils import timezone
+from .forms import updateForm
+@login_required
+def checking_renting(request):
+    renting_list=Warehouse.objects.filter(warehouse_currentuser_use_id=request.user.user_id)
+    return render(request, 'renter/checking_renting.html',{'renting_list':renting_list })
 
+@login_required
+def edit_info(request):
+    user = User.objects.get(user_id=request.user.user_id)
+    if request.method == 'POST':
+        user = User.objects.get(user_id=request.user.user_id)
+        user.email=request.POST.get('email')
+        user.first_name=request.POST.get('first_name')
+        user.last_name=request.POST.get('last_name')
+        user.save()
+        return render(request, 'renter/update_success.html')
+    else:
+        form = updateForm()
+    return render(
+        request,
+        'renter/edit_info.html',
+        {'user_info': user,"user_update":form})
 @login_required
 def renter_view(request):
     #template = loader.get_template('')
@@ -17,7 +39,7 @@ def renter_view(request):
 @login_required
 def renter_list(request):
     #html = Warehouse.warehouse_id
-    all_warehouses = Warehouse.objects.all()
+    all_warehouses = Warehouse.objects.filter(warehouse_isavailable=1)
     context = {'all_warehouses': all_warehouses}
 
     return render(request, 'renter/renter_list.html', context)
@@ -38,13 +60,11 @@ def payment_page(request,warehouse_id):
 def get_password(request,warehouse_id):
     TheWarehouse = Warehouse.objects.get(warehouse_id=warehouse_id)
     TheWarehouse.current_password = random.randint(0,999999999)
-    TheWarehouse.warehouse_currentowenr_use_id=request.user.user_id
+    TheWarehouse.warehouse_currentuser_use_id=request.user.user_id
+    TheWarehouse.warehouse_isavailable=0
     TheWarehouse.save()
     CurrentOrderId = random.randint(0,9999)
-    TheCurrentRent = RentOrder.objects.create(order_id=CurrentOrderId)
-    TheCurrentRent.user_id = request.user.user_id
-    today = datetime.today()
-    TheCurrentRent.order_date = today
+    TheCurrentRent = RentOrder.objects.create(order_id=CurrentOrderId,user_id=request.user.user_id,order_date=datetime.now())
     context = {'The_Warehouse': TheWarehouse , 'The_Current_Rent' : TheCurrentRent}
     return render(request, 'renter/get_password.html',  context)
 
