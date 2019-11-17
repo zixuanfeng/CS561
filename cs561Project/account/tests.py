a8a7d0ad3282b8e065a765c5a1f9d8ee2e3cef6e
McNew9527 <380582253@qq.com>
2019-11-16 16:41:39 -0800

    Add files via upload

--- cs561Project/account/tests.py
+++ cs561Project/account/tests.py
@@ -5,26 +5,39 @@ from django.test.client import Client
 # Create your tests here.
 class UserModelCase(TestCase):
     def setUp(self):
-        print("==Unit test for signUp")
+        print("==Unit test for ALL USER(LENDER AND RENTER) signUp")
         """
-       SignUp:{
-            "username": "test"
+        RenterSignUp:{
+            "username": "Rentertest"
             "password": "Asdf1029"
-            "email": "test@gmail.com"
+            "email": "Rentertest@gmail.com"
             "root": "1"
+        }
+        LenderSignUp:{
+            "username": "Lendertest"
+            "password": "Asdf1029"
+            "email": "Lendertest@gmail.com"
+            "root": "2"
         }
         """
         print("==User in setup")
-        User.objects.create( username='test', password='Asdf1029', email='test@gmail.com',user_id=1002)
-        self.login_user={'username':'testsignup','password':'Asdf1029'}
+        User.objects.create( username='Rentertest', password='Asdf1029', email='Rentertest@gmail.com',user_id=1002)
+        User.objects.create( username='Lendertest', password='Asdf1029', email='Lendertest@gmail.com',user_id=1003)
+        self.login_renter={'username':'Rentertest','password':'Asdf1029'}
+        self.login_lender={'username':'Lendertest','password':'Asdf1029'}
 
     def test_signUp(self):
        # response = self.client.get("signup_for_renter/")
-        response = self.client.post('login/', date = self.login_user)
+        response = self.client.post('login/', date = self.login_renter)
+        response = self.client.post('login/', date = self.login_lender)
         #self.assertEqual(response.status_code, 200)
        # self.assertIn('error',response.content)
-        res = User.objects.get(username='test')
-        print("Test User name is test and equal email with test@gamil.com ")
-        self.assertEqual(res.email,"test@gmail.com")
+        res1 = User.objects.get(username='Rentertest')
+        res2 = User.objects.get(username='Lendertest')
+        print("Test Renter name is Rentertest and equal email with Rentertest@gmail.com ")
+
+        print("Test Lender name is Lendertest and equal email with Lendertest@gmail.com ")
+        self.assertEqual(res1.email,"Rentertest@gmail.com")
+        self.assertEqual(res2.email,"Lendertest@gmail.com")
     def tearDown(self):
-        print("==User in tearDown")
+        print("==User data in tearDown")
